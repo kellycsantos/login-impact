8ae4982da2ac1720058c0d24715ad347b56124b9
Kelly Cristina dos Santos <95941924+kellycsantos@users.noreply.github.com>
2024-05-03 12:20:10 -0300

    Update Logincontainer.js

--- src/components/Logincontainer.js
+++ src/components/Logincontainer.js
@@ -8,7 +8,7 @@ function Container(){
         <div className={styles.container}>
         <img src={Logo} alt="genshim-impact"/>
        <Input type="text" label="Escolha seu nickname" placeholder="Moomo123" />
-       <Input type="email" label="Insira seu melhor e-mail" placeholder="moomoo@teste.com" />
+       <Input type="email" label="Insira seu melhor e-mail" placeholder="moomoo@gmail.com" />
        <Input type="password" label="Escolha uma senha forte" placeholder="******" />
         <SubmitButton value="Criar conta"/>
         <p>Já tem uma conta? <a href="/#">LOGIN</a></p>
